d7eebd913155cc85b386d47ae0c85a7670277e1e
Nyleesia Nicome Schnarr <nyleesia.nicome@users.noreply.github.com>
2019-10-03 16:31:42 -0400

    second commit: included cli table, connected to database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,49 +1,62 @@
 // Get dependencies
 const inquirer = require("inquirer");
-const mySQL = require("mysql");
+const mysql = require("mysql");
+const Table = require("cli-table2")
+
+// Save terminal output method as a variable
+const log = (console.log);
 
 // Create connection to mySQL
 const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "bamazon"
+    host: "localhost",
+    user: "root",
+    password: "1tylerfly",
+    database: "bamazon",
+    port: 3306
 });
 
 // Connect to the mysql server and database
-connection.connect(function(err) {
-  if (err) throw err;
-  ShowInventory();
+connection.connect((err, connected) => {
+    if (err){
+        log(`Connecting to inventory database...`)
+        log("There was an error connecting to the database")
+    }
+    else{
+        log(`Connecting to inventory database...`)
+        log(`Connection: Successful!`)
+    }
 });
 
 // Show the user the inventory listing so they see what is available
-function ShowInventory() {
-    connection.query("SELECT * FROM bamazon", function(err, data) {
-        if (err) throw err;
+let ShowInventory = function () {
+    connection.query("SELECT * FROM products", function(err, data ) {
+        if (err){
+            log(`Oops! Something has gone wrong here. Please try connecting later.`)
+        }
+        log("******************************************************");
+        log("******************Welcome to Bamazon******************");
+        log("******************************************************");
+        log("");
+        log("***********See our current inventory below************"); 
+        log(""); 
 
-    //Get information from user to determine order
-    inquirer
-        .prompt({
-            name: "inventory",
-            type: "rawlist",
-            choices: function() {
-                let inventoryArray = [];
-                let inventoryNames = [];
-                let inventoryPrices = [];
-                for (var i = 0; i < data.length; i++) {
-                    inventoryNames.push(data[i].product_name),  inventoryPrices.push(data[i].price), inventoryArray.push( inventoryNames), inventoryArray.push( inventoryPrices);
-                }
-                return inventoryArray;
-              },
-            message: "What would you like to order today?"
-        }).then(function(answer) {
-        // Acertain the quantity that the user wants to order
-
-            // Or end the connection 
-            {
-                connection.end();
-            }
-        });
+        // Instantiate cli table 2
+    let table = new Table({
+        head: ["Product ID", "Name", "Department", "Price", "In Stock"], 
+        colWidths: [8, 20, 20, 8, 8], 
+        colAligns: ["center", "left", "left", "center", "center"], 
+        style: {
+            head: ["aqua"], 
+            compact: true
+        } 
     });
-};
+
+    for (var i = 0; i < data.length; i++) {
+        table.push([data[i].item_id, data[i].product_name, data[i].department_name, data[i].price, data[i].stock_quantity]);
+    }
+
+    log(table.toString()); 
+    log("******************************************************");
+    });
+}; 
+ShowInventory()
